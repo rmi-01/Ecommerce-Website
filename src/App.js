42d4fd6afdb4e7411050827a3bf91256bb10e940
Ibrahim <rmibrahim00@gmail.com>
2020-10-16 23:25:24 +0500

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.js";
 import HomePage from "./pages/homepage/HomePage.js";
 import ShopPage from "./pages/shop/ShopPage.js";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/shop">
-        <ShopPage />
-      </Route>
-      <Route path="/">
-        <HomePage />
-      </Route>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/shop">
+          <ShopPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/header/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/4.3 crown.svg";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="shop">
+          SHOP
+        </Link>
+        <Link className="option" to="contact">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
