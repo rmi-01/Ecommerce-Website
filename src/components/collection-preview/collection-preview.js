0458d6bfcc4029924396dc17c44fa48e27ef696d
Ibrahim <rmibrahim00@gmail.com>
2020-10-16 23:02:35 +0500

    Adding preview items

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "./collection-preview.scss";
+import CollectionItem from "../collection-item/collection-item";
+import "./collection-preview.styles.scss";
 
 function CollectionPreview({ title, items }) {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map(({ id, name }) => (
-          <div key={id}>{name}</div>
-        ))}
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherProps }) => (
+            <CollectionItem key={id} {...otherProps} />
+          ))}
       </div>
     </div>
   );
