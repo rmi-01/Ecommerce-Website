bd7a2b3a773d68f889dafc956445757831a18759
Ibrahim <rmibrahim00@gmail.com>
2020-11-03 22:20:07 +0500

    Created cart-item.

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
+import { useSelector } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
+import { selectCartItems } from "../../redux/selectors";
+import CartItem from "../cart-item/cart-item";
 
 function CartDropdown() {
+  const cartItems = useSelector(selectCartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem item={cartItem} key={cartItem.id} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
