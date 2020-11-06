50ae814c5288ddaa3d7ea186dda420f0e40851ee
Ibrahim <rmibrahim00@gmail.com>
2020-11-06 18:39:20 +0500

    Few changes.

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-import { selectShopItems } from "../../redux/shop/shop.selector";
+import CollectionOverview from "../../components/collection-overview/collection-overview";
 
 function ShopPage() {
-  const productItems = useSelector(selectShopItems);
   return (
     <div className="shop-page">
-      {productItems.map(({ id, ...product }) => (
-        <CollectionPreview key={id} {...product} />
-      ))}
+      <CollectionOverview />
     </div>
   );
 }
