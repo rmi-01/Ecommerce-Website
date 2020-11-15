177db1c335b96f956298683a99914a7b87878738
Ibrahim <rmibrahim00@gmail.com>
2020-11-15 17:14:54 +0500

    Few changes.

--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useRouteMatch } from "react-router-dom";
+import { selectCollection } from "../../redux/shop/shop.selector";
 
 function CategoryPage() {
   const match = useRouteMatch();
+
+  const collection = useSelector((state) =>
+    selectCollection(match.params.collectionId)(state)
+  );
+
   return <div className="category">CATEGORY PAGE</div>;
 }
 
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,9 +1,20 @@
 import { createSelector } from "@reduxjs/toolkit";
 
+const mapCollectionToId = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
-//state.product.productsList
-export const selectShopItems = createSelector(
-  selectShop,
-  (product) => product.items
-);
+//state.shop.items
+export const selectShopItems = createSelector(selectShop, (shop) => shop.items);
+
+//state.shop.items(specific for collection page)
+export const selectCollection = (category) =>
+  createSelector(selectShopItems, (items) =>
+    items.find((item) => item.id === mapCollectionToId[category])
+  );
