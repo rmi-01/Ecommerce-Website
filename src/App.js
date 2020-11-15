81c12aa93112be0886f4b0aeda49cb918d7b6e74
Ibrahim <rmibrahim00@gmail.com>
2020-11-15 21:34:29 +0500

    Fewing naming changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.js";
-import HomePage from "./pages/homepage/homepage.js";
+import HomePage from "./pages/homepage/HomePage.js";
 import ShopPage from "./pages/shop/shop.js";
 import "./app.css";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.js";
